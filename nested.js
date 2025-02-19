3cdc456290208e5b2d8d7876ff00562196876126
abdullah9755 <abdullahrifat9755@gmail.com>
2025-02-19 23:04:02 +0600

    nested condition

---
+++ nested.js
@@ -0,0 +1,20 @@
+const money = 0;
+if(money>300)
+{
+    console.log("Bro you are richer than me")
+}
+else{
+    if(money>100)
+    {
+        console.log("You are middle class");
+    }
+    else{
+        if(money>0)
+        {
+            console.log("you can only buy banana");
+        }
+        else{
+            console.log("i don't have money to give you");
+        }
+    }
+}
